dfdd1a056125bc8513e7f44378a239d6e724ff72
wyd <w_yidan@163.com>
2020-03-09 22:59:09 +0800

    上传build

---
+++ react-todo/my-app/build/precache-manifest.fac59cda2b0abe34b8961ffa270dcbb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8458b927e9680febb225b2378615c28d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7bddf13f0c66e404ef6c",
+    "url": "./static/css/main.c2c572ad.chunk.css"
+  },
+  {
+    "revision": "3ac75588c52edbd5efb9",
+    "url": "./static/js/2.0ed368b2.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "./static/js/2.0ed368b2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7bddf13f0c66e404ef6c",
+    "url": "./static/js/main.a534826f.chunk.js"
+  },
+  {
+    "revision": "353824c915518b56d5bd",
+    "url": "./static/js/runtime-main.1130748e.js"
+  }
+]);
